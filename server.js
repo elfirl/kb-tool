e22c415c813503863d6d0c06bf32d874f154424a
Mike Bilter <mike@stratisiot.com>
2018-06-27 14:55:14 -0400

    Added a routes and a test route.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+
 const app = express();
 
 // DB Config
@@ -13,6 +16,10 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello!"));
+
+// Use Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
